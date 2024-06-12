70fa2718246aea4edf37e9e0a399c42b03ff6ec0
unknown <samuel.imex@gmail.com>
2024-06-12 22:42:25 +0100

    added test for shortlink controller

--- src/shortlink/shortlink.controller.spec.ts
+++ src/shortlink/shortlink.controller.spec.ts
@@ -1,18 +1,74 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ShortlinkController } from './shortlink.controller';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../app.module';
+import { ShortlinkService } from './shortlink.service';
 
-describe('ShortlinkController', () => {
-  let controller: ShortlinkController;
+describe('ShortlinkController (e2e)', () => {
+  let app: INestApplication;
+  let shortlinkService: ShortlinkService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [ShortlinkController],
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
     }).compile();
 
-    controller = module.get<ShortlinkController>(ShortlinkController);
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    shortlinkService = moduleFixture.get<ShortlinkService>(ShortlinkService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/shortlink/encode (POST) should encode a URL', async () => {
+    const longUrl = 'https://indicina.co';
+    const response = await request(app.getHttpServer())
+      .post('/shortlink/encode')
+      .send({ url: longUrl })
+      .expect(201);
+
+    expect(response.body).toHaveProperty('shortUrl');
+    const shortUrl = response.body.shortUrl;
+    expect(shortUrl).toMatch(/^http:\/\/short\.est\/\w+$/);
+
+    // Verify the URL was stored correctly
+    const urlHash = shortUrl.replace('http://short.est/', '');
+    const storedUrl = shortlinkService.getStatistics(urlHash);
+    expect(storedUrl.longUrl).toBe(longUrl);
+  });
+
+  it('/shortlink/decode (POST) should decode a short URL', async () => {
+    const longUrl = 'https://indicina.co';
+    const shortUrl = shortlinkService.encode(longUrl);
+
+    await request(app.getHttpServer())
+      .post('/shortlink/decode')
+      .send({ url: shortUrl })
+      .expect(302)
+      .expect('Location', longUrl);
+  });
+
+  it('/shortlink/statistic/:urlPath (GET) should return URL statistics', async () => {
+    const longUrl = 'https://indicina.co';
+    const shortUrl = shortlinkService.encode(longUrl);
+    const urlHash = shortUrl.replace('http://short.est/', '');
+
+    const response = await request(app.getHttpServer())
+      .get(`/shortlink/statistic/${urlHash}`)
+      .expect(200);
+
+    expect(response.body).toHaveProperty('longUrl', longUrl);
+    expect(response.body).toHaveProperty('shortUrl', shortUrl);
+    expect(response.body).toHaveProperty('visits', 0);
+    expect(response.body).toHaveProperty('createdAt');
+  });
+
+  it('/shortlink/statistic/:urlPath (GET) should return 404 for non-existent URL', async () => {
+    await request(app.getHttpServer())
+      .get(`/shortlink/statistic/nonExistent`)
+      .expect(404);
   });
 });
